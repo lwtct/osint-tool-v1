fa9c36359eda94cf7dcf9cec2cb4380c0cca6a5b
Evann <lwtctgithub@gmail.com>
2019-08-11 23:54:21 +0200

    Post args

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__,static_url_path='', static_folder='static')
 
 @app.route("/")
 def root():
-    output = "Result"
+    output = request.args.get("input")
     return render_template("index.html", output=output)
 
 
