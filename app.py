da869337598222cfd0946e52033ae4b901019bf9
delta <c380927219c95b4b93657f85f87b3a8b>
2019-08-23 14:24:22 +0200

    started on json data integration

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__,static_url_path='', static_folder='static')
 
-file = open('data/resources.json', 'r')
+with open('data/resources.json', 'r') as resources:
+    data = json.load(resources)
+    #add json input
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    text = request.form['output']
-    return render_template("index.html", output=text)
+    output_text = request.form['output']
+    input_text = request.form['input']
+    if output_text == 'Adress':
+        output_number=1
+        print(1)
+    elif output_text == 'Phone Number':
+        output_number=2
+        print(2)
+    if input_text == 'Name':
+        input_number=8
+        print(8)
+    elif input_text == 'Email':
+        input_number = 1
+        print(1)
+
+    # idea; loop over all the resources and look for anything with matching input and output tag.
+    server_output=input_text+output_text #use for debugging, remove later
+    return render_template("index.html", output=server_output)
 
 @app.route("/")
 def root():
