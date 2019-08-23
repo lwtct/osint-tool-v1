caf9709f3f13ddd2e942f8e3622f14c3fba37cf6
delta <c380927219c95b4b93657f85f87b3a8b>
2019-08-23 12:54:43 +0200

    added feedback functionality and a resources folder + file

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__,static_url_path='', static_folder='static')
 
-#HI THERE THIS IS A MERGE TEST
+file = open('data/resources.json', 'r')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['output']
+    return render_template("index.html", output=text)
 
 @app.route("/")
 def root():
     output = request.args.get("input")
     return render_template("index.html", output=output)
-
 
 app.run(debug=True)
