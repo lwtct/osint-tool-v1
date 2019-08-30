b7ffb79b54c43cab05c971b0749fd54fd01e1d49
delta <c380927219c95b4b93657f85f87b3a8b>
2019-08-30 08:59:34 +0200

    updated search_database() to include descriptions

--- app.py
+++ app.py
@@ -5,43 +5,54 @@ app = Flask(__name__,static_url_path='', static_folder='static')
 
 
 def search_database(input_number, output_number):
-    pav_list = [] #defines the output list
-    with open('data/resources.json', 'r') as resources: #opens the json file under working name 'resources'
-        data = json.loads(resources.read()) #dumps the JSON data into a list
-        data_counter = 0 #resets the variable
-        for line in data: #iterates over each entry of list 'data'
+    pav_list = []  # defines the URL output list
+    des_list = []  # defines the discription output list
+    with open('data/resources.json', 'r') as resources:  # opens the json file under working name 'resources'
+        data = json.loads(resources.read())  # dumps the JSON data into a list
+        data_counter = 0  # resets the variable
+        for line in data:  # iterates over each entry of list 'data'
             try:
-                if 'url' in str(data[data_counter]): #checks if the entry is a valid resource entry
-                    input_confirm = 0 #reset variables
-                    output_confirm = 0
-                    work = str(data[data_counter])  #creating a string of the active list entry
-                    work = work.split("', ")  #creating a list of the string 'work'
-                    work_counter = 0 #reset variables
+                if 'url' in str(data[data_counter]):  # checks if the entry is a valid resource entry
+                    work = str(data[data_counter])  # creating a string of the active list entry
+                    work = work.split("', ")  # creating a list of the string 'work'
+                    work_counter = 0  # reset variables
                     list_item_check_input = 0
                     list_item_check_output = 0
-                    for list_item in work:  #iterate over each entry of the list 'work'
-                        if 'input' in str(work[work_counter]):  #check if the current entry in list 'work' contains the input classification
-                            work_input = str(work[work_counter]) #save the location of the input classification for later use
+                    list_item_check_purpose = 0
+                    list_item_check_url = 0
+                    for list_item in work:  # iterate over each entry of the list 'work'
+                        if 'input' in str(work[work_counter]):   # check if the current entry in list 'work' contains the input classification
+                            work_input = str(work[work_counter])  # save the location of the input classification for later use
                             list_item_check_input = 1
-                        if 'output' in str(work[work_counter]): #does what was done for input previously for output
+                        if 'output' in str(work[work_counter]):  # does what was done for input previously for output
                             work_output = str(work[work_counter])
                             list_item_check_output = 1
-                        if list_item_check_output == 1 & list_item_check_input == 1: #if both the input and output location are found there is no reason to continue checking
+                        if 'purpose' in str(work[work_counter]):
+                            purpose = str(work[work_counter])
+                            list_item_check_purpose = 1
+                        if 'url' in str(work[work_counter]):
+                            url = str(work[work_counter])
+                            list_item_check_url = 1
+                        if list_item_check_output == 1 & list_item_check_input == 1 & list_item_check_purpose == 1 & list_item_check_url == 1:  # if all the location are found there is no reason to continue checking
                             pass
-                        work_counter += 1 #updates the current entry of list work
-                    if str(input_number) in work_input and str(output_number) in work_output: #check if the active entry has the necessary input and output classification
-                        pav = str(work[0])  #variable name from Pyro57#6998. Feel free to annoy him on discord even though he did next to nothing with the development of this application.
-                        pav = pav.replace("{'url': '", "") #removes the unnecessary part and just leaves the URL
-                        pav_list.append(pav) #adds the URL to the string
+                        work_counter += 1  # updates the current entry of list work
+                    if str(input_number) in work_input and str(output_number) in work_output:  # check if the active entry has the necessary input and output classification
+                        pav = url  # variable name from Pyro57#6998. Feel free to annoy him on discord even though he did next to nothing with the development of this application.
+                        pav = pav.replace("{'url': '", "")  # removes the unnecessary part and just leaves the URL
+                        pav_list.append(pav)  # adds the URL to the output list
+                        des = purpose
+                        des = des.replace("'purpose': '", "")  # removes unnecessary part and just leaves the description
+                        des_list.append(des)  # adds the description to the output list
                     else:
                         pass
                 else:
                     pass
-            except Exception:     #deals with the errors
+            except Exception:  # deals with the errors
                 pass
-            data_counter += 1 #moves on to next entry in list 'data'
-    return pav_list #when completed this returns a list of all URLs with the correct classification
+            data_counter += 1  # moves on to next entry in list 'data'
+    return pav_list, des_list  # when completed this returns a list of all URLs and description with the correct classification
 
+print (search_database(1, 5))
 querry_type = {
     'Email' : 1,
     'Home address' : 2,
